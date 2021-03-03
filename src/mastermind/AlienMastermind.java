073319d0f08019dcb560bdf7d918c2390a21afad
Emily Navarro <emilyo@uci.edu>
2021-03-03 05:00:58 -0800

    removed unnecessary variable to remove compiler warning

--- src/mastermind/AlienMastermind.java
+++ src/mastermind/AlienMastermind.java
@@ -1,11 +1,10 @@
 package mastermind;
 
 public class AlienMastermind {
-	private GameBoard board;
 	private static final int DEFAULT_LEVEL = 1;
 
 	public void execute() {
-		board = new GameBoard(DEFAULT_LEVEL);
+		new GameBoard(DEFAULT_LEVEL);
 	}
 	
 	public static void main(String args[]) {
